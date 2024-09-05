4225bda712fc18e601551fe5258e1f1ebb907f88
Facundo Vergara <fakumax@gmail.com>
2024-09-04 21:13:41 -0300

    Proyecto listo para arrancar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function App() {
 	return (
-		<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-			<Text>¡Bienvenido a la App!</Text>
-		</View>
+		<NavigationContainer>
+			<View style={styles.container}>
+				<Text>Este es el Header</Text>
+			</View>
+		</NavigationContainer>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+});
